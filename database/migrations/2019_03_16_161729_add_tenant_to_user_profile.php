5356a911dc84eb3ff6104469a8b9de112da6aa4f
dennismwagiru <dennismwagiru@gmail.com>
2019-03-16 19:27:18 +0300

    update - Add tenant_id to user_profile

---
+++ database/migrations/2019_03_16_161729_add_tenant_to_user_profile.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddTenantToUserProfile extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('user_profiles', function (Blueprint $table) {
+            $table->unsignedInteger('tenant_id')->nullable()->after('user_id');
+            $table->foreign('tenant_id')->references('id')->on('tenants')->onDelete('cascade')->onUpdate('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
